df455c23f8dd2726ef7e2f0eb791ba194f4eeba2
Arjis2020 <arjis.chakraborty@gmail.com>
2023-03-07 23:57:04 +0530

    removed fs manipulation

--- bot.js
+++ bot.js
@@ -2,8 +2,7 @@ import { Bot, webhookCallback, InputFile } from "grammy";
 import express from "express";
 import { config } from "dotenv";
 import ytdl from "ytdl-core";
-// import { MemoryWriter, readFromMemory } from "./utils/memory.js";
-import fs from 'fs'
+import { MemoryWriter, readFromMemory } from "./utils/memory.js";
 
 config()
 
@@ -14,28 +13,23 @@ bot.command('audio', async (message) => {
     const chatId = message.chat.id
 
     if (ytdl.validateURL(url)) {
-        // const mw = new MemoryWriter(chatId)
+        const mw = new MemoryWriter(chatId)
 
         await bot.api.sendMessage(chatId, "Received your request. Please wait for a couple of minutes. This shouldn't take long :)")
         const info = await ytdl.getInfo(url)
         const title = info.videoDetails.title
 
-        const ws = fs.createWriteStream(`./outputs/audio/${chatId}.mp3`)
-        ws.on('open', () => {
-            const registration = ytdl(url, {
-                filter: 'audioonly'
-            })
-            registration.pipe(ws)
-            // registration.on('data', function (chunk) {
-            //     console.log("data") 
-            //     mw.writeChunk(chunk) 
-            // })
-            registration.on('end', async function () {
-                console.log("done")
-                await bot.api.sendAudio(chatId, new InputFile(fs.createReadStream(`./outputs/audio/${chatId}.mp3`), title))
-                // mw.close()
-                ws.close()
-            })
+        const registration = ytdl(url, {
+            filter: 'audioonly'
+        })
+        registration.on('data', function (chunk) {
+            console.log("data") 
+            mw.writeChunk(chunk) 
+        })
+        registration.on('end', async function () {
+            console.log("done")
+            await bot.api.sendAudio(chatId, new InputFile(readFromMemory(chatId), title))
+            mw.close()
         })
     }
     else {
@@ -43,7 +37,7 @@ bot.command('audio', async (message) => {
     }
 })
 
-if (process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
     const app = express()
     const PORT = process.env.PORT || 9000
 
